9c2e30d4b9bca9f1f12d0ee00b5a92f68f057c03
Audran DITSCH <aditsch@e1r4p1.42.fr>
2016-11-17 12:04:45 +0100

    redo ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: aditsch <aditsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/14 01:28:01 by aditsch           #+#    #+#             */
-/*   Updated: 2016/11/05 14:03:59 by aditsch          ###   ########.fr       */
+/*   Updated: 2016/11/17 12:03:32 by aditsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ret;
 
-	ret = NULL;
-	if (!(s1 && s2))
-		return (ret);
-	ret = ft_strnew((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (ret)
-		ft_strcat(ft_strcpy(ret, s1), s2);
+	if (!((s1 && s2) && (ret = ft_strnew((ft_strlen(s1) + ft_strlen(s2)) + 1))))
+		return (NULL);
+	ft_strcat(ft_strcpy(ret, s1), s2);
 	return (ret);
 }
